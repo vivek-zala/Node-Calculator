bc9ff57ae9b47a55036b5443ff28ddce0a159ace
vivek-zala <vzala@nsforce.in>
2024-11-28 14:16:59 +0530

    Add additinal subtract test case which should fail

--- calculator.test.js
+++ calculator.test.js
@@ -1,13 +1,9 @@
 const { add, subtract, multiply, divide } = require('./calculator');
 
+// Base testcases
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
-
-// Added additional addition test case.
-test('adds 1 + 3 to equal 4', () => {
-    expect(add(1, 3)).toBe(4);
-  });
 
 test('subtracts 5 - 3 to equal 2', () => {
   expect(subtract(5, 3)).toBe(2);
@@ -24,3 +20,13 @@ test('divides 6 / 2 to equal 3', () => {
 test('throws error when dividing by zero', () => {
   expect(() => divide(6, 0)).toThrow("Cannot divide by zero");
 });
+
+// Added additional addition test case. (Should success)
+test('adds 1 + 3 to equal 4', () => {
+    expect(add(1, 3)).toBe(4);
+  });
+
+// Added additinal subtract test case. (Should fail)
+  test('subtracts 5 - 3 to equal 1', () => {
+    expect(subtract(5, 3)).toBe(1);
+  });
