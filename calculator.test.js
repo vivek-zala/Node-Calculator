43520a7bb7b9036d71cea44c9df0f57ab76fbb67
vivek-zala <vzala@nsforce.in>
2024-11-28 14:13:13 +0530

    Add addition extra test case

--- calculator.test.js
+++ calculator.test.js
@@ -4,6 +4,11 @@ test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
 
+// Added additional addition test case.
+test('adds 1 + 3 to equal 4', () => {
+    expect(add(1, 3)).toBe(4);
+  });
+
 test('subtracts 5 - 3 to equal 2', () => {
   expect(subtract(5, 3)).toBe(2);
 });
